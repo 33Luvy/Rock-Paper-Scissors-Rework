0d06bd33ae87e89a0a969e4519cee5354bf0190e
33Luvy <33luvy@gmail.com>
2023-03-27 14:55:55 +0100

    Added a function for computer choice

---
+++ jscript.js
@@ -0,0 +1,10 @@
+
+// Function for computer choice
+function getComputerChoice() {
+    // Array with 3 choices
+    choice = ["rock", "paper", "scissors"];
+    // Returns the variable as one of strings from the array
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+
+console.log(getComputerChoice())
